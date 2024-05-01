fa193df377d858e844627ca8bbe49cb6aa773762
Leon <junzai12@icloud.com>
2024-04-30 21:42:03 -0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 let active: boolean = false;
 
+const baseLink = () => window.location.href = "/";
+
 const hamburgerMenu = () => {
   active ? document.getElementById("hamburger")?.classList.add("active") : document.getElementById("hamburger")?.classList.remove("active");
   active ? document.getElementById("menu")!.style.display = "block" : document.getElementById("menu")!.style.display = "none";
@@ -11,10 +13,10 @@ const Navbar = () => {
     <div className="">
       <nav>
         <ul id="nav-list">
-        <li><a className="nav" onClick={() => window.location.href = "/"} href="/">Home</a></li>
-        <li><a className="nav" onClick={() => window.location.href = "/"} href="#about">About</a></li>
-        <li><a className="nav" onClick={() => window.location.href = "/"} href="#works">Works</a></li>
-        <li><a className="nav" onClick={() => window.location.href = "/"} href="#contact">Contact</a></li>
+        <li><a className="nav" onClick={baseLink} href="/">Home</a></li>
+        <li><a className="nav" onClick={baseLink} href="#about">About</a></li>
+        <li><a className="nav" onClick={baseLink} href="#works">Works</a></li>
+        <li><a className="nav" onClick={baseLink} href="#contact">Contact</a></li>
         <li><a className="nav" onClick={() => window.location.href = "blog.html"}>Blog</a></li>
         </ul>
         <div id="hamburger" onClick={() => hamburgerMenu()}>
