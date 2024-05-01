607b4b51139654fe4d0ed903921429273a60ada1
Leon <junzai12@icloud.com>
2024-04-30 23:40:53 -0700

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,6 +13,8 @@ const Home = () => {
             <div className="flex">
             <img id="room" src={room} alt="A room in Blender 3D by me." width="600" />
             </div>
+            <h1>Leon Truong</h1>
+            <h1>張嘉俊</h1>
         </div>
     )
 }
