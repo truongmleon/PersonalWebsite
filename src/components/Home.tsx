bc5df576f898e11dfe2279f41d46f7eaa693940f
Leon <junzai12@icloud.com>
2024-05-01 22:31:00 -0700

    name things

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,8 +13,8 @@ const Home = () => {
             <div className="flex">
             <img id="room" src={room} alt="A room in Blender 3D by me." width="600" />
             </div>
-            <h1>Leon Truong</h1>
-            <h1>張嘉俊</h1>
+            <h1 id="name-eng">Leon Truong</h1>
+            <h2 id="name-cn">張嘉俊</h2>
         </div>
     )
 }
