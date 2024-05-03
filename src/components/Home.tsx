0171c60b18962196c1e4adb33a6d5012472d4a65
Leon <junzai12@icloud.com>
2024-05-02 21:55:26 -0700

    navbar design no longer fixed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
             <div className="flex">
             <img id="room" src={room} alt="A room in Blender 3D by me." width="600" />
             </div>
-            <h1 id="name-eng">Leon Truong</h1>
+            <h1 id="name-eng">Leon Truong!</h1>
             <h2 id="name-cn">張嘉俊</h2>
         </div>
     )
