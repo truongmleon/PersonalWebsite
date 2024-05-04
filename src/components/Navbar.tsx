c69e11f3dadee12624f69d63d8397ecfc337abe4
Leon <junzai12@icloud.com>
2024-05-03 20:55:26 -0700

    navbar works as intended

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,9 +14,9 @@ const Navbar = () => {
       <nav>
         <ul id="nav-list">
         <li><a className="nav" onClick={baseLink} href="/">Home</a></li>
-        <li><a className="nav" onClick={baseLink} href="#about">About</a></li>
-        <li><a className="nav" onClick={baseLink} href="#works">Works</a></li>
-        <li><a className="nav" onClick={baseLink} href="#contact">Contact</a></li>
+        <li><a className="nav" href="#about">About</a></li>
+        <li><a className="nav" href="#works">Works</a></li>
+        <li><a className="nav" href="#contact">Contact</a></li>
         <li><a className="nav" onClick={() => window.location.href = "blog.html"}>Blog</a></li>
         </ul>
         <div id="hamburger" onClick={() => hamburgerMenu()}>
