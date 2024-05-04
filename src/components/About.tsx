500d37ee72310b7e4b365c01242df565b7cb759d
Leon <junzai12@icloud.com>
2024-05-04 00:31:16 -0700

    about me details

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,19 @@ const About = () => {
         <div id="about">
             <br/> 
             <h3 id="introduction">About me</h3>
-            <p></p>
+            <p className="blurb-me">Hi, I’m Leon. I’m a self-taught web developer in React.js and hobbyist 3D modeler with Blender 3D. 
+                I’m attending the University of Washington majoring in computer science. 
+                Since the summer before my sophomore year of high school, I’ve been learning how to code to create whatever weird and impractical things I can think of. 
+            </p>
+            <p className="blurb-me">
+                Outside of coding, I enjoy playing video games (Persona, MC, AC:NH), strategizing chess (challenge me!), and reading manga (view my favs). 
+                I’m an avid listener for J-pop and sometimes K-pop from my friends (view playlists).
+            </p>
+            <p className="blurb-me">
+                With so much learning when it comes to different types of technologies, I developed an intense passion for learning EVERYTHING. 
+                I have great friends who brought me to this point.
+                You can learn and receive more insight about me in my blog :)).
+            </p>
         </div>
     )
 }
