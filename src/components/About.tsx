b17b5bae7fe17a38cf6c5756af5462b8eb991151
Leon <junzai12@icloud.com>
2024-05-27 16:04:18 -0700

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 const getImage = () => {
     const img: HTMLElement | null = document!.getElementById("games");
-    const num: number = Math.floor(Math.random() * 21 + 1);
+    const num: number = Math.floor(Math.random() * 33 + 1);
     img!.setAttribute("src", `/src/assets/switch/${num}.png`);
 }
 
