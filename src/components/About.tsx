976997af399312fe098f3330850c555e4a644e61
Leon <junzai12@icloud.com>
2024-05-27 09:43:14 -0700

    image gimmikc works

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,7 @@
-const img = document.getElementById("games");
-
 const getImage = () => {
-    
-    
+    const img: HTMLElement | null = document!.getElementById("games");
+    const num: number = Math.floor(Math.random() * 21 + 1);
+    img!.setAttribute("src", `/src/assets/switch/${num}.png`);
 }
 
 const About = () => {
