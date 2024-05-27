d58300ee59fef6f65a0fa6c86ef2058339ac6fb4
Leon <junzai12@icloud.com>
2024-05-26 20:28:30 -0700

    about me design

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,11 @@
-import gears from "/src/assets/gear.png";
+import games from "/src/assets/games.png";
 
 const About = () => {
     return (
         <div id="about">
             <br/> 
-            <img id="gears" src={gears} alt="Gears made in Blender by me." />
-            <h3 id="introduction">About me</h3>
+            <img id="games" src={games} alt="Nintendo Switch and Game Boy made in Blender by me." />
+            <h3 id="introduction">About me _(ツ)_/¯</h3>
             <p className="blurb-me">Hi, I’m Leon. I’m a self-taught web developer in React.js and hobbyist 3D modeler with Blender 3D. 
                 I’m attending the University of Washington majoring in computer science. 
                 Since the summer before my sophomore year of high school, I’ve been learning how to code to create whatever weird and impractical things I can think of. 
