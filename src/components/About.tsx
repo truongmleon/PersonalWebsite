c87b947e4f91280a25d36156c009a871f97d6f2b
Leon <junzai12@icloud.com>
2024-05-27 20:55:57 -0700

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,43 @@
-import { switchImages } from "../Images";
+import image1 from "/src/assets/switch/1.png";
+import image2 from "/src/assets/switch/2.png";
+import image3 from "/src/assets/switch/3.png";
+import image4 from "/src/assets/switch/4.png";
+import image5 from "/src/assets/switch/5.png";
+import image6 from "/src/assets/switch/6.png";
+import image7 from "/src/assets/switch/7.png";
+import image8 from "/src/assets/switch/8.png";
+import image9 from "/src/assets/switch/9.png";
+import image10 from "/src/assets/switch/10.png";
+import image11 from "/src/assets/switch/11.png";
+import image12 from "/src/assets/switch/12.png";
+import image13 from "/src/assets/switch/13.png";
+import image14 from "/src/assets/switch/14.png";
+import image15 from "/src/assets/switch/15.png";
+import image16 from "/src/assets/switch/16.png";
+import image17 from "/src/assets/switch/17.png";
+import image18 from "/src/assets/switch/18.png";
+import image19 from "/src/assets/switch/19.png";
+import image20 from "/src/assets/switch/20.png";
+import image21 from "/src/assets/switch/21.png";
+import image22 from "/src/assets/switch/22.png";
+import image23 from "/src/assets/switch/23.png";
+import image24 from "/src/assets/switch/24.png";
+import image25 from "/src/assets/switch/25.png";
+import image26 from "/src/assets/switch/26.png";
+import image27 from "/src/assets/switch/27.png";
+import image28 from "/src/assets/switch/28.png";
+import image29 from "/src/assets/switch/29.png";
+import image30 from "/src/assets/switch/30.png";
+import image31 from "/src/assets/switch/31.png";
+import image32 from "/src/assets/switch/32.png";
+import image33 from "/src/assets/switch/33.png";
+
+ const switchImages = [
+    image1, image2, image3, image4, image5, image6, image7, image8, image9, image10,
+    image11, image12, image13, image14, image15, image16, image17, image18, image19, image20,
+    image21, image22, image23, image24, image25, image26, image27, image28, image29, image30,
+    image31, image32, image33
+];
 
 const getImage = () => {
     const img: HTMLElement | null = document!.getElementById("games");
