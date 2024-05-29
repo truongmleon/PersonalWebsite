014d0e72226e64517f8e6af70c5d2fb1cf8ac311
Leon <junzai12@icloud.com>
2024-05-28 22:45:31 -0700

    Update Works.tsx

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -2,7 +2,9 @@ const Works = () => {
     return (
         <div>
             <br/>
-            <h2>Works</h2>  
+            <div className="flex">
+            <h2 id="works-title">Programming Projects</h2>
+            </div>  
 
         </div>
     )
