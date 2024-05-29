fdadb2b3dfcbbb298ab357aa92bd83baa9ebd0e8
Leon <junzai12@icloud.com>
2024-05-28 20:59:29 -0700

    seeing if this is better

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,9 @@
-import room from "/src/assets/room.png";
-
 const Home = () => {
     return (
         <div id="home">
             
             <div className="flex">
-                <img id="room" src={room} alt="A room in Blender 3D by me." width="600" />
+                <img id="room" src="/assets/room.png" alt="A room in Blender 3D by me." width="600" />
             </div>
             <h1 id="name-eng">Leon Truong</h1>
             <h2 id="name-cn">張嘉俊</h2>
--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,5 @@ export default defineConfig({
       },
     },
   },
-  publicDir: 'src/assets'
+  publicDir: 'public'
 })
