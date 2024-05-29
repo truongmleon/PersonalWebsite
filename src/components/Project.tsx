4d4e44168310d7c71650c5cc0711a6dd77e3961c
Leon <junzai12@icloud.com>
2024-05-29 16:48:48 -0700

    box shadow fix

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,8 +1,8 @@
-const Project = ({image, title, link} : {image: string, title: string, link: string}) => {
+const Project = ({image, title, link, id} : {image: string, id: string, title: string, link: string}) => {
     return (
         <section className="project-box">
             <h3 className="project-title">{title}</h3>
-            <img className="project" src={"/projects/default/" + image} alt={title} onClick={() => window.open(link, "_blank")} />
+            <img id={id} className="project" src={`/projects/default/${image}`} alt={title} onClick={() => window.open(link, "_blank")} />
         </section>
     )
 }
