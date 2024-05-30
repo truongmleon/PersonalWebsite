f7d68d97f77aefe7c58912844f22c290f1c45a88
Leon <junzai12@icloud.com>
2024-05-29 21:51:34 -0700

    Update Works.tsx

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -13,9 +13,9 @@ const Works = () => {
                     I try to create good designs in my projects.
                 </h3>
                 <section id="project-grid">
-                <Project image={"WarringNations.png"} id="warring-nations" title={"Warring Nations Game"} link={"https://github.com/truongmleon/Warring-Nations-MoreMathEdition"}/>
-                <Project image={"Waves.png"} id="ap-physics" title={"AP Physics Video"} link={"https://github.com/truongmleon/Playground/tree/small-projects/AP%20Physics%201%3A%20Video%20Project"}/>
-                <Project image={"KeyClub.png"} id="key-club" title={"Key Club Website"} link={"https://github.com/truongmleon/Key-Club-Website"}/>
+                <Project image={"WarringNations.webp"} id="warring-nations" title={"Warring Nations Game"} link={"https://github.com/truongmleon/Warring-Nations-MoreMathEdition"}/>
+                <Project image={"Waves.webp"} id="ap-physics" title={"AP Physics Video"} link={"https://github.com/truongmleon/Playground/tree/small-projects/AP%20Physics%201%3A%20Video%20Project"}/>
+                <Project image={"KeyClub.webp"} id="key-club" title={"Key Club Website"} link={"https://github.com/truongmleon/Key-Club-Website"}/>
                 </section>
             </section>
             </div>
