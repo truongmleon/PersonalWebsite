d2ed85b28855ad703f6a02b7cc3a8660f73b8ec6
Leon <junzai12@icloud.com>
2024-05-29 21:33:33 -0700

    webp is so much faster

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,14 @@
 const getImage = () => {
     const img: HTMLElement | null = document!.getElementById("games");
     const num: number = Math.floor(Math.random() * 33 + 1);
-    img!.setAttribute("src", `/switch/${num}.png`);
+    img!.setAttribute("src", `/switch/${num}.webp`);
 }
 
 const About = () => {
     return (
         <section id="about">
             <br/> 
-            <img id="games" onClick={() => getImage()} src="/switch/1.png" alt="Nintendo Switch and Game Boy made in Blender by me." />
+            <img id="games" onClick={() => getImage()} src="/switch/1.webp" alt="Nintendo Switch and Game Boy made in Blender by me." />
             <h3 id="introduction">About me _(ツ)_/¯</h3>
             <p className="blurb-me">Hi, I’m Leon. I’m a self-taught web developer in React.js and hobbyist 3D modeler with Blender 3D. 
                 I’m attending the University of Washington majoring in computer science. 
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -16,7 +16,6 @@ const Works = () => {
                 <Project image={"WarringNations.png"} id="warring-nations" title={"Warring Nations Game"} link={"https://github.com/truongmleon/Warring-Nations-MoreMathEdition"}/>
                 <Project image={"Waves.png"} id="ap-physics" title={"AP Physics Video"} link={"https://github.com/truongmleon/Playground/tree/small-projects/AP%20Physics%201%3A%20Video%20Project"}/>
                 <Project image={"KeyClub.png"} id="key-club" title={"Key Club Website"} link={"https://github.com/truongmleon/Key-Club-Website"}/>
-
                 </section>
             </section>
             </div>
