c3dd0f474d7f9ac0d99de2958c974929b7e2bc71
Leon <junzai12@icloud.com>
2024-05-30 00:41:22 -0700

    style changes

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -2,7 +2,7 @@ const Blog = () => {
     return (
     <article className="flex">
         <section className="sections">
-        <h2 className="section-title">Blog</h2> 
+        <h2 className="section-title">Bloooooooog</h2> 
         <h3 className="context">
             Empty so far.
         </h3>
--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -3,7 +3,7 @@ const Connect = () => {
         <article className="flex">
             <br/>
             <section className="sections">
-            <h2 className="section-title">Connect</h2> 
+            <h2 className="section-title">Connect w/ me :3</h2> 
             <h3 className="context">
                 Have a question? Wanna be friends?
             </h3>
