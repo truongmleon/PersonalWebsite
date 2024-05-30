52793154f871d390c2c3b1e1445885fa3e535eac
Leon <junzai12@icloud.com>
2024-05-29 21:01:59 -0700

    works page finished

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,16 +1,9 @@
-const hoverActive = (id: string) => {
-    const defaultImage = document.getElementById(id);
-    const hoveredImage = document.getElementById(`${id}-hovered`);
-    hoveredImage!.style.opacity = "0";
-    return "";
-}
-
-const Project = ({image, title, link, id} : {image: string, id: string, title: string, link: string}) => {
+const Project = ({image, id, title, link} : {image: string, id: string, title: string, link: string}) => {
     return (
         <section className="project-box">
             <h3 className="project-title">{title}</h3>
-            <img id={id} className="project" src={`/projects/default/${image}`} alt={title} onClick={() => window.open(link, "_blank")} />
-            <img id={`${id}-hover`} className="project" src={`/projects/hover/${image}`} alt={title} onClick={() => window.open(link, "_blank")} />
+            <img id={`${id}-hover`} className="project hidden" src={`/projects/hover/${image}`} alt={title} onClick={() => window.open(link, "_blank")} />
+            <img id={id} className="project active" src={`/projects/default/${image}`} alt={title} onClick={() => window.open(link, "_blank")} />
         </section>
     )
 }
