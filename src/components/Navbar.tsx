60d13c4472799c2db02fc1e5fdfea5a5d894370c
Leon <junzai12@icloud.com>
2025-06-19 19:38:04 -0700

    works to progs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <nav>
         <ul id="nav-list">
         <li><a className="nav" onClick={baseLink} href="/">Home</a></li>
-        <li><a className="nav" href="works.html"> Works</a></li>
+        <li><a className="nav" href="projects.html"> Projects</a></li>
         </ul>
       </nav>
     </div>
