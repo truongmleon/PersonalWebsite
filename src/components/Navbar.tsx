c80194461162a15189694200bb2becc009805b6e
Leon <junzai12@icloud.com>
2025-06-19 19:35:51 -0700

    removing blogs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,4 @@
-let active: boolean = false;
-
 const baseLink = () => window.location.href = "/";
-
-const hamburgerMenu = () => {
-  active ? document.getElementById("hamburger")?.classList.add("active") : document.getElementById("hamburger")?.classList.remove("active");
-  active ? document.getElementById("menu")!.style.display = "block" : document.getElementById("menu")!.style.display = "none";
-  active = !active;
-}
 
 const Navbar = () => {
   return (
@@ -15,16 +7,7 @@ const Navbar = () => {
         <ul id="nav-list">
         <li><a className="nav" onClick={baseLink} href="/">Home</a></li>
         <li><a className="nav" href="works.html"> Works</a></li>
-        <li><a className="nav" href="blog.html">&nbsp; Blog</a></li>
-        <li><a className="nav" href="connect.html">Connect</a></li>
         </ul>
-        <div id="hamburger" onClick={() => hamburgerMenu()}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        </div>
-        <div id="menu">
-      </div>
       </nav>
     </div>
   )
