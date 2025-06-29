e28b66d060901ef32f2ebaa97ede513830e5651a
Leon <junzai12@icloud.com>
2025-06-29 12:55:56 -0700

    deployment

--- src/components/Ribbons/Navbar.tsx
+++ src/components/Ribbons/Navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <div className="">
       <nav>
         <ul id="nav-list">
-        <li><a className="nav" onClick={() => window.location.href = "/"} href="/">Home</a></li>
+        <li><a className="nav" onClick={() => window.location.href = "."} href="/">Home</a></li>
         <li><a className="nav" href="projects.html">Projects</a></li>
         </ul>
       </nav>
